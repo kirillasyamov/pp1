7b9da4e26c3912b5894714cde949d1ce0e7b5119
Kirill Asyamov <104299015+metilteobromin@users.noreply.github.com>
2022-05-11 18:00:06 +0600

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const PORT = process.env.PORT || 5000;
-const app = express();
-app.listen(PORT, () => console.log(`WORKING ON ${PORT}`));
-
-
-
-
